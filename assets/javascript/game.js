cab5b0748624bf1c5c6c95ed7c1d1ecfc69d4f7a
emma lapalm <lillypadcrafts@gmail.com>
2020-05-05 12:22:35 -0700

    adjusted varraibles to start game functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,16 @@
 
 var words = ["PHONEIX", "SAGUARO", "COYOTE", "MONSOON", "DRY HEAT", "GRAND CANYON", "RATTLE SNAKE", "MOUNTAINS", "SEDONA", "MARGARITAS", "SUNSHINE"]
 
-// scoring varriables
 
-var spaces = 0;
-var letters = []
-var randomWord = "";
-var wrongGuess = 0;
-var spacesAndWord = [];
-var blankSpaces = 0;
-
-//
+var totalGuesses = 12; // Total guesses possible
+var lettersGuesed = []; // holds the guessed letters
+var randomWord = []; //Pulls a word frim the array
+var remainingGuesses = 0; // Counts the number of remaining guesses
 var wins = 0;
 var losses= 0;
-var remainingGuesses = 9;
+var gameEnd = false; //When the game ends (is true); game restarts
+var wordAnswer; //Answer is shown when guesses = 0;
+
 
 
 // for display
@@ -25,7 +22,7 @@ var remainingGuesses = document.getElementById("remaining-guesses")
 
 // game start
 
-function GameStart {
+function GameStart() {
 
 var randomWord = words[Math.floor(Math.random() * words.length)];
   letters = randomWord.split("");
@@ -33,19 +30,21 @@ var randomWord = words[Math.floor(Math.random() * words.length)];
   blanks = letters.length;
   
   for (var i = 0; 1 < blanks; i++) {
-    spacesAndWord.push("_";
+    spacesAndWord.push("_");
     {
 
+      document.getElementById("currentword").innerHTML = " " + blankSpaces.join(" ");
 // computer randomly displays a word in space
-document.textContent(randomWord)
+document.textContent(randomWord)                
   
   document.onkeyup = function(event) {
 var letters = event.key;
+  }
+}
 }
 };
-
 // player guesses a letter. if guess is right the computer will replace the correct space with the word. 
 // if the guess is wrong the guessed letter will go into the "wrong bin" and player loses a guess
 // if the player uses all guesses and does not guess the word the game is over and the correct word is displayed
 // if the player guesses the correct word an image supporting the word appears and a sound will play.
-// game restarts after sound ends and a new word is displayed. 
+// game restarts after sound ends and a new word is displayed
